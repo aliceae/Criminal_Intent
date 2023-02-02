36f7f96ab501067b9f13cf8d1488d29b7a6e2f58
alice <aliceae.h@gmail.com>
2023-02-02 13:44:46 +0800

    [ch13]setup the navigation

--- app/src/main/java/com/dev/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/dev/criminalintent/CrimeDetailFragment.kt
@@ -1,15 +1,17 @@
 package com.dev.criminalintent
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.dev.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.*
 
-
+private const val TAG = "CrimeDetailFragement"
 class CrimeDetailFragment : Fragment() {
     //    private lateinit var binding: FragmentCrimeDetailBinding
     private var _binding: FragmentCrimeDetailBinding? = null
@@ -18,6 +20,7 @@ class CrimeDetailFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
     private lateinit var crime: Crime
+    private val args: CrimeDetailFragmentArgs by navArgs()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime(
@@ -26,6 +29,7 @@ class CrimeDetailFragment : Fragment() {
             date = Date(),
             isSolved = false
         )
+        Log.d(TAG,"The crime ID is: ${args.crimeId}")
     }
 
     override fun onCreateView(
