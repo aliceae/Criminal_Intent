dd6abbc5003984a53b0107e389ec80ff5df4ed60
alice <aliceae.h@gmail.com>
2023-03-24 15:01:34 +0800

    Fragments - add Fragment

---
+++ app/src/main/java/com/dev/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,55 @@
+package com.dev.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.dev.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.*
+
+
+class CrimeDetailFragment : Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
+    private lateinit var crime: Crime
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            UUID.randomUUID(),
+            "",
+            Date(),
+            false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener{ _, isChecked->
+                crime = crime.copy(isSovlved = isChecked)
+            }
+        }
+    }
+
+}
